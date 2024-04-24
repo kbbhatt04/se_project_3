82a73b2e67bdf429f2970418e6b50733863bdd60
Madhusree Bera <madhusreebera1999@gmail.com>
2024-04-25 00:43:30 +0530

    added load balancing logic

--- microservices/load_balancer_subsystem/load_balancer_subsystem.py
+++ microservices/load_balancer_subsystem/load_balancer_subsystem.py
@@ -58,6 +58,57 @@ async def register_service(service: Service):
     return {"message": "Service instance registered successfully!"}
 
 
+async  def route_request(service_type, instance, body, path):
+    service_url = instance['service_url'] + "/" + path
+    if service_type == "GET":
+        response = requests.get(service_url)
+    elif service_type == "POST":
+        response = requests.post(service_url, json=body)
+    return response.json()
+
+class Request(BaseModel):
+    service_type: str #GET, POST
+    service_name: str #service name
+    body: Optional[dict] = None
+    path: str
+
+@app.post("/balance_load")
+async def balance_load(request: Request):
+    service_name = request.service_name
+    service_type = request.service_type
+    body = request.body
+    path = request.path
+
+    #getting available instances
+    db = get_db()
+    service_collection = db["service_registry"]
+    instances = service_collection.find({"service_name": service_name}, {"deleted": False})
+    instances = list(instances)
+    instances_count = len(instances)
+
+    if instances_count == 0:
+        logger.log(message=f"No instances found for {service_name} service!", level='error')
+        return {"message": "No instances found for the service!"}
+    else:
+        logger.log(message=f"Total instances found for {service_name} service: {instances_count}", level='info')
+        
+        load_balancing_collection = db["load_balancing"]
+        load_balancing = load_balancing_collection.find_one({"service_name": service_name})
+        instances_index = 0
+        if load_balancing:
+            instances_index = load_balancing['last_instance_index']
+            instances_index = (instances_index + 1) % instances_count #simple round robin
+            load_balancing_collection.update_one({"service_name": service_name}, {"$set": {"last_instance_index": instances_index}})
+        else:
+            load_balancing_collection.insert_one({"service_name": service_name, "last_instance_index": instances_index})
+
+        instance = instances[instances_index]
+        
+        logger.log(message=f"Load balanced to {service_name} instance with ID: {instance['id']}", level='info')
+
+        return route_request(service_type, instance, body, path)
+
+
 if __name__ == "__main__":
     import uvicorn
 
